813fcddc436ee650624b1ff0091892dd151e4b1d
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-08-28 20:57:49 +0530

    sonar-user_and password

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,50 +1,64 @@
-def call () {
+def call() {
+
+//    if(!env.SONAR_EXTRA_OPTS) {
+//        env.SONAR_EXTRA_OPTS = " "
+//    }
+//
+//
+//    if(!env.extra_files) {
+//        env.extra_files = " "
+//    }
+//
+//    if(!env.TAG_NAME) {
+//        env.PUSH_CODE = "false"
+//    } else {
+//        env.PUSH_CODE = "true"
+//    }
+
     try {
-        pipeline {
+        node('workstation') {
 
-            agent {
-                label 'workstation'
+            stage('Scripted Checkout:SCM') {
+                cleanWs()
+                git branch: 'main', url: "https://github.com/chandra5141/${component}.git"
+                sh 'env'
             }
 
-            stages {
-                stage('Compile/Build') {
-                    steps {
-                        script {
-                            common.compile()
-                        }
-                    }
-                }
+            stage('Compile/Build') {
+                common.compile()
+            }
 
-                stage('Unit_tests') {
-                    steps {
-                        script {
-                            common.unit_test()
-                        }
-                    }
-                }
+            stage('Unit Tests') {
+                common.unit_test()
+            }
 
-                stage('Quality_control') {
-                    environment {
-                        SONAR_USER = '$(sudo aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        SONAR_PASS = '$(sudo aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                    }
-                    steps {
-                        sh  "sonar-scanner -Dsonar.host.url=http://172.31.94.206:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${JOB_BASE_NAME}"
-                    }
-                }
-
-                stage('Upload the artifact to central repo') {
-                    steps {
-                        echo "artifact upload"
-                    }
+            stage('Quality Control') {
+                SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.94.206:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                    sh "echo Sonar Scan"
                 }
             }
+
+
+//            if (app_lang == "maven") {
+//                stage('Build Package') {
+//                    sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
+//                }
+//            }
+//
+//
+//            if(env.PUSH_CODE == "true") {
+//                stage('Upload Code to Centralized Place') {
+//                    common.artifactpush()
+//                }
+//            }
 
 
         }
-    } catch (Exception e) {
-        common.email("failed")
+
+    } catch(Exception e) {
+        common.email("Failed")
     }
-
 }
-
---
+++ vars/cideclarative.groovy
@@ -0,0 +1,50 @@
+def call () {
+    try {
+        pipeline {
+
+            agent {
+                label 'workstation'
+            }
+
+            stages {
+                stage('Compile/Build') {
+                    steps {
+                        script {
+                            common.compile()
+                        }
+                    }
+                }
+
+                stage('Unit_tests') {
+                    steps {
+                        script {
+                            common.unit_test()
+                        }
+                    }
+                }
+
+                stage('Quality_control') {
+                    environment {
+                        SONAR_USER = '$(sudo aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(sudo aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
+                    steps {
+                        sh  "sonar-scanner -Dsonar.host.url=http://172.31.94.206:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${JOB_BASE_NAME}"
+                    }
+                }
+
+                stage('Upload the artifact to central repo') {
+                    steps {
+                        echo "artifact upload"
+                    }
+                }
+            }
+
+
+        }
+    } catch (Exception e) {
+        common.email("failed")
+    }
+
+}
+
